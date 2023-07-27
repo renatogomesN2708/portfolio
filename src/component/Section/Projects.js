90c722adf3154c65b3632122803a1b9277670bd7
renatogomesN2708 <wellenvitoria.1904@gmail.com>
2023-07-27 19:28:59 -0300

    Aplicando responsividade

--- src/component/Section/Projects.js
+++ src/component/Section/Projects.js
@@ -10,9 +10,11 @@ function Projects() {
     return (
         <div className={style.projects} id="projects">
             <h1>Projetos</h1>
-            <C3/>
-            <C2/>
-            <Card1/>
+            <div>
+                <C3/>
+                <C2/>
+                <Card1/>
+            </div>
             <section>
             <ButtonB text='Acesse meu repositorio' link='https://github.com/renatogomesN2708'></ButtonB>
             </section>
